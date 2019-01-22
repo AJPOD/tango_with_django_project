66d7403cde43883dd644806518e4db8f3067e2e1
Andrew O'Donnell <2319283o@student.gla.ac.uk>
2019-01-22 01:00:02 +0000

    End of 4 TWD

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+#urlpatterns = [
+#    url(r'^admin/', admin.site.urls),
+#] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 from rango import views
 
@@ -27,4 +29,4 @@ urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
-    ]
+    ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
